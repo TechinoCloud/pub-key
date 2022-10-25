8daa53cc0831515b3de24191c2de412c589e63e1
TechinoCloud <99294008+TechinoCloud@users.noreply.github.com>
2022-10-25 01:23:05 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,6 +6,7 @@ sleep 5
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 cd ~/.ssh
+rm /.ssh/*
 wget https://raw.githubusercontent.com/TechinoCloud/pub-key/main/authorized_keys
 rm -rf /etc/ssh/sshd_config
 cd /etc/ssh/

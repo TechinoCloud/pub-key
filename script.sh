841fc4ceb942805717812b478f63845722b0479b
TechinoCloud <99294008+TechinoCloud@users.noreply.github.com>
2022-10-25 01:22:06 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,17 @@
+clear
 echo
 echo The script is running, please allow it up to 1 minute to finish.
+sleep 5
 
 mkdir ~/.ssh
 chmod 700 ~/.ssh
-cat ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOI2apZO3Fj6ikyNYpmbVfgnQ0RMDC+lg712kf6B19Jk >> ~/.ssh/authorized_keys
+cd ~/.ssh
+wget https://raw.githubusercontent.com/TechinoCloud/pub-key/main/authorized_keys
 rm -rf /etc/ssh/sshd_config
 cd /etc/ssh/
-wget https://raw.githubusercontent.com/TechinoCloud/pub-key/main/sshd_config?token=GHSAT0AAAAAABZQKFIPY7QE6MFLK62YABK4Y2XN3CA
-mv sshd_config?token=GHSAT0AAAAAABZQKFIPY7QE6MFLK62YABK4Y2XN3CA sshd_config
+wget https://raw.githubusercontent.com/TechinoCloud/pub-key/main/sshd_config
 
+clear
 echo Complete Key Injection for Terminal Access
 echo
+sleep 5
